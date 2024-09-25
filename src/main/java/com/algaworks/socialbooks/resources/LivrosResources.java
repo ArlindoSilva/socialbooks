d260d76f2ba152b0e46e85a99807d0b9c1301eda
ArlindoSilva <arlindoas2@gmail.com>
2024-09-24 22:25:07 -0300

    Criando o primeiro recurso e os métodos salvar e buscar

---
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,35 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
+
+@RestController
+@RequestMapping("/livros")
+public class LivrosResources {
+
+	@Autowired
+	private LivrosRepository livrosRepository;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
+    }
+
+	@RequestMapping(method = RequestMethod.POST)
+	//RequestBody para poder fazer o parse e colocar as informações desse do objeto livro
+	public void salvar(@RequestBody Livro livro){
+		livrosRepository.save(livro);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id){
+		return this.livrosRepository.findById(id).orElse(null);
+	}
+	
+}
