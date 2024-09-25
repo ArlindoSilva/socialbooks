fd59a936e6fc05257f83acdd8ebec58bef001146
ArlindoSilva <arlindoas2@gmail.com>
2024-09-25 20:49:21 -0300

    tratamento de respostas no método buscar

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,13 +1,16 @@
 package com.algaworks.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/livros")
@@ -23,13 +26,23 @@ public class LivrosResources {
 
 	@RequestMapping(method = RequestMethod.POST)
 	//RequestBody para poder fazer o parse e colocar as informações desse do objeto livro
-	public void salvar(@RequestBody Livro livro){
-		livrosRepository.save(livro);
+	public ResponseEntity<?> salvar(@RequestBody Livro livro){
+		livro = livrosRepository.save(livro);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id){
-		return this.livrosRepository.findById(id).orElse(null);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
+		Livro livro = livrosRepository.findById(id).orElse(null);
+		if(livro == null){
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
+		//return this.livrosRepository.findById(id).orElse(null);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
