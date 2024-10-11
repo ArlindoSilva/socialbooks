793bce86b05166e709a7ff01272a911d1cf2c0b9
ArlindoSilva <arlindoas2@gmail.com>
2024-10-11 15:53:26 -0300

    alterações necessárias para a api-client ter acesso aos dados pelo Browser

--- src/main/java/com/algaworks/socialbooks/config/SecurityConfig.java
+++ src/main/java/com/algaworks/socialbooks/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,12 +38,20 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
                 .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
+
+
+/*Para o Chrome poder ter acesso aos dados e preencher a tabela,
+* foi preciso adicionar o options(linha 41), e permitir todos os endereços.
+* Além da anotação @CrossOrigin no método listar de LivrosResources.
+*
+* Isso foi necessário para a api-client poder acessar a api principal */
 
 
 
